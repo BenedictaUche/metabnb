dc1dae95094702804e047fa0446b2d5d3ca0d20e
Benedicta <benedictauche.onyebuchi@gmail.com>
2022-11-14 23:40:39 +0100

    add: added styling to the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,47 +1,51 @@
 import React, { useState } from 'react';
 import MediaQuery from 'react-responsive';
-import Navbar from './Navbar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAlignRight, faTimes } from '@fortawesome/free-solid-svg-icons';
 import logo from '../imgs/metaLogo.png';
 import Button from './Button';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-const Header = ({ setModal }) => {
+import Navbar from './Navbar';
+
+const Header = ({ handleModal }) => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const handleMenuOpen = () => {
-        setIsMenuOpen(!isMenuOpen);
-    };
-    return ( 
-        <header>
-            <div>
-                <img src={logo} alt="Metabnb Logo" />
-            </div>
-            <MediaQuery maxWidth={768}>
-                {matches => matches ? (
-                    <> {isMenuOpen ? (
-                        <Button type="button" onClick={handleMenuOpen} isMenuOpen>
-                            <FontAwesomeIcon icon={faTimes} size='1x' />
-                        </Button>
-                    ) : (
-                        <Button type="button" onClick={handleMenuOpen}>
-                            <FontAwesomeIcon icon={faAlignRight} size='1x' />
-                        </Button>
-                    )
-                    }
-                    <Navbar menuOpen={isMenuOpen} handleMenuOpen={handleMenuOpen}/>
-                    </>
-                ) : 
-                (
-                    <>
-                    <Navbar menuOpen={isMenuOpen} handleMenuOpen={handleMenuOpen}/>
-                    </>
-                )
-                }
-            </MediaQuery>
-            <Button type="button" onClick={() => setModal('modal-active')}>
-                Connect Wallet
-            </Button>
-        </header>
-     );
+		setIsMenuOpen(!isMenuOpen);
+	};
+
+  return (
+    <header className='w-full h-20 lg:h-24 fixed top-0 left-0 right-0 px-5 py-5 lg:px-20 bg-whites flex justify-between items-center z-10'>
+      <div className='w-36 lg:w-56'>
+        <img src={logo} alt="logo" />
+      </div>
+      <MediaQuery maxWidth={1024}>
+        {matches =>
+          matches ? (
+            <>
+              {isMenuOpen ? (
+			          <Button type="button" onClick={handleMenuOpen} isMenuOpen>
+				          <FontAwesomeIcon icon={faTimes} size='2x' className="text-pink"/>
+				        </Button>
+				      ) : (
+				        <Button type="button" onClick={handleMenuOpen}>
+				          <FontAwesomeIcon icon={faAlignRight} size='2x' className="text-pink"/>
+				        </Button>
+				      )
+              }
+
+              <Navbar menuOpen={isMenuOpen} handleMenuOpen={handleMenuOpen} handleModal={handleModal}/>
+
+            </>
+          ) :
+          (
+            <>
+             <Navbar handleModal={handleModal}/>
+            </>
+          )
+        }
+        </MediaQuery>
+    </header>
+
+  )
 }
- 
-export default Header;
+
+export default Header
